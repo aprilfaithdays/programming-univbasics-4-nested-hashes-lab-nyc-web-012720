b17f481211f44f0afe1d969ba9839c2397e2dbaa
April Escobar <aprilfaith.e@gmail.com>
2020-01-10 08:50:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,8 +109,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:known_for] = "GUI"
-     programmer_hash
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -129,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  programmer_hash
 end
